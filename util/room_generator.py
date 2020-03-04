42c9018efa56a1c5059ac41d67fe09c12d4e38d0
shenry45 <shawn45henry@gmail.com>
2020-03-03 21:55:52 -0700

    room generation link to Room model and Room objects save to db on creation

--- util/room_generator.py
+++ util/room_generator.py
@@ -1,26 +1,7 @@
+import math, sys
 from random import random
-import math
 from .roomList import names
-
-class Room:
-    def __init__(self, id, name, description, secret, n_room, e_room, s_room, w_room):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.secret = secret
-        self.n_room = n_room
-        self.e_room = e_room
-        self.s_room = s_room
-        self.w_room = w_room
-
-    def __str__(self):
-        print(f'${self.name} is ${self.description}')
-
-    def connect_room(self, connecting_room, dir):
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[dir]
-        setattr(self, f"{dir}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
+from adventure.models import Room
 
 class World:
     def __init__(self):
@@ -28,10 +9,11 @@ class World:
         self.players = ()
 
     def generate_rooms(self, count):
-        start_room = Room(0, 'Foyer', 'A dingy foyer where the floor is filled with torn papers of an unknown book. What happened here?', None, None, None, None, None)
-        # insert start room
-        self.rooms.append(start_room)
-        # count_iter -= 1
+        start_room = Room(
+            title = 'Foyer',
+            description = 'A dingy foyer where the floor is filled with torn papers of an unknown book. What happened here?'
+        )
+        start_room.save()
         current_room = start_room
 
         # create rooms by looping up to count number
@@ -39,11 +21,13 @@ class World:
             random_room = names[math.floor(len(names) * random())]
             print(i, random_room)
             # Create room
-            new_room = Room(i, random_room[0], random_room[1], None, None, None, None, current_room)
-            # mark new_room as next room
-            current_room.e_room = new_room
-            # insert room to world
-            self.rooms.append(new_room)
+            new_room = Room(
+                title = random_room[0],
+                description = random_room[1]
+            )
+            # mark new_to as next room
+            current_room.connectRooms(new_room, 'e')
+            new_room.save()
             # change current_room
             current_room = new_room
 
