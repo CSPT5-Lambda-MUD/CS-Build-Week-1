7f8a16b607834161768b85cbc2a97ac358f72be1
shenry45 <shawn45henry@gmail.com>
2020-03-04 20:36:14 -0700

    comment sample room generators

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
         'PORT': config('PGPORT')
     },
     'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
