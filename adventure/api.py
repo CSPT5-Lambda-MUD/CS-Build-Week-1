8b5abc30dfced98cf92d7aed4823da885184b7e9
the-best-developer <im.dylan.gosnell@gmail.com>
2020-03-06 23:32:29 -0500

    set up grid data to be used with frontend

--- adventure/api.py
+++ adventure/api.py
@@ -4,9 +4,86 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from decouple import config
 from django.contrib.auth.models import User
-from .models import *
+from adventure.models import *
 from rest_framework.decorators import api_view
 import json
+
+
+
+
+class World:
+    def __init__(self):
+        self.grid = None
+        self.width = 0
+        self.height = 0
+
+    def generate_rooms(self, size_x, size_y, num_rooms):
+        '''
+        Fill up the grid, bottom to top, in a zig-zag pattern
+        '''
+
+        # Initialize the grid
+        self.grid = [None] * size_y
+        self.width = size_x
+        self.height = size_y
+        for i in range( len(self.grid) ):
+            self.grid[i] = [None] * size_x
+
+        # Start from lower-left corner (0,0)
+        x = size_x // 2 # (this will become 0 on the first step)
+        y = size_y // 2
+        room_count = 0
+
+        # Start generating rooms to the east
+        direction = 1  # 1: east, -1: west
+
+
+        # While there are rooms to be created...
+        previous_room = None
+        while room_count < num_rooms:
+
+            # Calculate the direction of the room to be created
+            if direction > 0 and x < size_x - 1:
+                room_direction = "e"
+                x += 1
+            elif direction < 0 and x > 0:
+                room_direction = "w"
+                x -= 1
+            else:
+                # If we hit a wall, turn north and reverse direction
+                room_direction = "n"
+                y += 1
+                direction *= -1
+            
+            
+
+            # Create a room in the given direction
+            room = Room(id=room_count, title="A Generic Room", description="This is a generic room.", x=x, y=y)
+            room.save()
+            # Note that in Django, you'll need to save the room after you create it
+
+            # Save the room in the World grid
+            self.grid[y][x] = room
+
+            # Connect the new room to the previous room
+            if previous_room is not None:
+                previous_room.connectRooms(room, room_direction)
+
+            # Update iteration variables
+            previous_room = room
+            room_count += 1
+
+w = World()
+num_rooms = 30
+width = 10
+height = 10
+w.generate_rooms(width, height, num_rooms)
+
+players=Player.objects.all()
+for p in players:
+  p.currentRoom = Room.objects.first().id
+  p.save()
+
 
 # instantiate pusher
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
@@ -23,7 +100,7 @@ def initialize(request):
     return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
 
 
-# @csrf_exempt
+@csrf_exempt
 @api_view(["POST"])
 def move(request):
     dirs={"n": "north", "s": "south", "e": "east", "w": "west"}
@@ -69,10 +146,35 @@ def say(request):
 
 @api_view(["GET"])
 def room(request):
-    all_rooms = Room.objects.all()
-    room_list = []
-    for room in all_rooms:
-        print(room)
-        # Adds each room object to an array in a better format
-        room_list.append({'id': room.id, 'title': room.title, 'desc': room.description, 'n': room.n_to, 's': room.s_to, 'e': room.e_to, 'w': room.w_to})
-    return JsonResponse({'rooms': room_list}, safe=True)
+    new_list = []
+    for x in range(len(w.grid)):
+        for y in range(len(w.grid[x])):
+            if w.grid[x][y] != None:
+                if request.user.player.currentRoom == w.grid[x][y].id:
+                    new_list.append({
+                        "name": w.grid[x][y].title,
+                        "n" : w.grid[x][y].n_to,
+                        "s" : w.grid[x][y].s_to,
+                        "e" : w.grid[x][y].e_to,
+                        "w" : w.grid[x][y].w_to,
+                        "x" : w.grid[x][y].x,
+                        "y" : w.grid[x][y].y,
+                        "current_room": True
+                    })
+                else:
+                    new_list.append({
+                        "name": w.grid[x][y].title,
+                        "n" : w.grid[x][y].n_to,
+                        "s" : w.grid[x][y].s_to,
+                        "e" : w.grid[x][y].e_to,
+                        "w" : w.grid[x][y].w_to,
+                        "x" : w.grid[x][y].x,
+                        "y" : w.grid[x][y].y,
+                        "current_room": False
+                })
+            else:
+                new_list.append("0")
+
+
+    return JsonResponse({'rooms': new_list}, safe=True)
+
